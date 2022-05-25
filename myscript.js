4c361aedb5c458246b23248baa10da9b166c5bfb
bcat4490 <bcat4490@gmail.com>
2022-05-24 18:36:10 -0700

    implemented getRandomElement

--- myscript.js
+++ myscript.js
@@ -6,3 +6,18 @@ blockButton.addEventListener('click', (e) => {
     e.target.style.background = 'blue';
 });
 
+let elements = ["a", "abbr", "acronym", "audio", "b", "bdi", "bdo", "big", "br", "button", "canvas", "cite", "code", "data", "datalist", "del", "dfn", "em", "embed", "i", "iframe", "img", "input", "ins", "kbd", "label", "map", "mark", "meter", "noscript", "object", "output", "picture", "progress", "q", "ruby", "s", "samp", "script", "select", "slot", "small", "span", "strong", "sub", "sup", "svg", "template", "textarea", "time", "u", "tt", "var", "video", "wbr", "address", "article", "aside", "blockquote", "details", "dialog", "dd", "div", "dl", "dt", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hgroup", "hr", "li", "main", "nav", "ol", "p", "pre", "section", "table", "ul" ];
+
+
+function getRandomInt(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandomElement(arr) {
+let index = getRandomInt(0, arr.length);
+return arr[index];
+}
+
+console.log(getRandomElement(elements));
