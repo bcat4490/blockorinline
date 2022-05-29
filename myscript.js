bc5e47cdc0fe741f5e154b311daac9d6f7a8896a
bcat4490 <bcat4490@gmail.com>
2022-05-28 19:15:10 -0700

    added reset button and attached event listener to update the to-be-guessed tag when the button is clicked

--- myscript.js
+++ myscript.js
@@ -1,10 +1,3 @@
-const blockButton = document.querySelector('#button-block');
-
-const inlineButton = document.querySelector('#inline-button');
-
-blockButton.addEventListener('click', (e) => {
-    e.target.style.background = 'blue';
-});
 
 let elements = ["a", "abbr", "acronym", "audio", "b", "bdi", "bdo", "big", "br", "button", "canvas", "cite", "code", "data", "datalist", "del", "dfn", "em", "embed", "i", "iframe", "img", "input", "ins", "kbd", "label", "map", "mark", "meter", "noscript", "object", "output", "picture", "progress", "q", "ruby", "s", "samp", "script", "select", "slot", "small", "span", "strong", "sub", "sup", "svg", "template", "textarea", "time", "u", "tt", "var", "video", "wbr", "address", "article", "aside", "blockquote", "details", "dialog", "dd", "div", "dl", "dt", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "header", "hgroup", "hr", "li", "main", "nav", "ol", "p", "pre", "section", "table", "ul" ];
 
@@ -50,5 +43,17 @@ function updateGuessElement(guessId, randomId) {
 appendRandomElement('footer', 'randomElement', elements);
 
 updateGuessElement('tag', 'randomElement');
+
+
+console.log(document.getElementById('randomElement'));
+
+let resetButton = document.getElementById('reset-button');
+
+resetButton.addEventListener('click', () => {
+    removeById('randomElement');
+    appendRandomElement('footer', 'randomElement', elements);
+    updateGuessElement('tag', 'randomElement');
+
+});
 
 
